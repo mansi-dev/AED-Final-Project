9decdda97cfb0177ace779c45f51b4dd3201bdb7
unknown <mishra.ma@northeastern.edu>
2021-12-04 17:54:43 -0500

    GovernmentModelInitial

--- src/Business/EcoSystem.java
+++ src/Business/EcoSystem.java
@@ -6,7 +6,7 @@
 package Business;
 
 
-import Business.Customer.CustomerDirectory;
+import Business.Government.LicenseOrganization;
 import Business.DeliveryMan.DeliveryManDirectory;
 import Business.Restaurant.RestaurantDirectory;
 import Business.Role.Role;
@@ -21,10 +21,10 @@ public class EcoSystem extends Organization{
     
     private static EcoSystem business;
     private RestaurantDirectory restaurantDirectory;
-    private CustomerDirectory customerDirectory;
+    private LicenseOrganization customerDirectory;
     private DeliveryManDirectory deliveryManDirectory;
 
-    public EcoSystem(RestaurantDirectory restaurantDirectory, CustomerDirectory customerDirectory, DeliveryManDirectory deliveryManDirectory) {
+    public EcoSystem(RestaurantDirectory restaurantDirectory, LicenseOrganization customerDirectory, DeliveryManDirectory deliveryManDirectory) {
 
         this.restaurantDirectory = restaurantDirectory;
         this.customerDirectory = customerDirectory;
---
+++ src/Business/Government/LicenseOrganization.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Government;
+
+import java.util.Date;
+
+/**
+ *
+ * @author harold
+ */
+public class LicenseOrganization {
+
+    public int getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(int licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getLicenseName() {
+        return licenseName;
+    }
+
+    public void setLicenseName(String licenseName) {
+        this.licenseName = licenseName;
+    }
+
+    public Date getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(Date issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Long getIssueNumber() {
+        return licenseNumber;
+    }
+
+    public void setIssueNumber(Long licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+    
+    private int licenseId;
+    private String licenseName;
+    private Date issueDate;
+    private Date expiryDate;
+    private Long licenseNumber;
+
+    public LicenseOrganization(int licenseId, String licenseName, Date issueDate, Date expiryDate, Long licenseNumber) {
+        this.licenseId = licenseId;
+        this.licenseName = licenseName;
+        this.issueDate = issueDate;
+        this.expiryDate = expiryDate;
+        this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "LicenseOrganization{" + "licenseId=" + licenseId + ", licenseName=" + licenseName + ", issueDate=" + issueDate + ", expiryDate=" + expiryDate + ", licenseNumber=" + licenseNumber + '}';
+    }
+    
+        
+}
