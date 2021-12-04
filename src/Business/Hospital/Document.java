e9a367cd070c9ad330e37cdbbdd4bd49400c66e4
mansi-dev <zope.m@northeastern.edu>
2021-12-04 18:34:53 -0500

    Added hospital enterprise and Doctor, document organization

---
+++ src/Business/Hospital/Document.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Hospital;
+
+/**
+ *
+ * @author mansizope
+ */
+public class Document {
+    public void addMedicalRecord(){
+        
+    }
+
+    public void updateMedicalRecord(){
+        
+    }
+    
+    public void assignDoctor(){
+        
+    }
+    
+}
